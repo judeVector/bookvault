2c8ef20d5ec6f1dafd8570b0e693d6d1d39d44bd
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-09-18 21:17:16 +0400

    Update the frontend api views to handle messaging using rabbitmq

--- frontend_api/api/views.py
+++ frontend_api/api/views.py
@@ -1,6 +1,5 @@
 from django.db import transaction
 from django.shortcuts import get_object_or_404
-import requests
 
 from rest_framework import generics, serializers
 from django_filters.rest_framework import DjangoFilterBackend
@@ -8,30 +7,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .models import User, Book, Borrow
 from .serializers import UserSerializer, BookSerializer, BorrowSerializer
 from .filters import BookFilter
-from .helpers import publish_user_creation_message
-
-
-# class UserCreateView(generics.CreateAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-
-#     def perform_create(self, serializer):
-#         user = serializer.save()
-#         admin_api_url = "http://localhost:7000/api/users/create/"
-#         headers = {"Content-Type": "application/json"}
-#         data = {
-#             "email": user.email,
-#             "first_name": user.first_name,
-#             "last_name": user.last_name,
-#             "created_at": user.created_at.isoformat(),
-#         }
-
-#         try:
-#             response = requests.post(admin_api_url, json=data, headers=headers)
-#             response.raise_for_status()
-#         except requests.exceptions.RequestException as e:
-#             print(f"Error creating user in Admin API: {e}")
-#             raise serializers.ValidationError("Failed to notify the Admin API.")
+from .utils.publisher import publish_message_to_queue, publish_book_borrowing_message
 
 
 class UserCreateView(generics.CreateAPIView):
@@ -40,7 +16,15 @@ class UserCreateView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         user = serializer.save()
-        publish_user_creation_message(user)
+
+        data = {
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "created_at": user.created_at.isoformat(),
+        }
+
+        publish_message_to_queue("user_created", data)
 
 
 class BookListView(generics.ListAPIView):
@@ -55,6 +39,51 @@ class BookDetailView(generics.RetrieveAPIView):
     serializer_class = BookSerializer
 
 
+# class BorrowBookView(generics.CreateAPIView):
+#     queryset = Borrow.objects.all()
+#     serializer_class = BorrowSerializer
+
+#     @transaction.atomic
+#     def perform_create(self, serializer):
+#         book_id = self.kwargs.get("book_id")
+#         book = get_object_or_404(Book, pk=book_id)
+
+#         if book.available:
+#             book.available = False
+#             book.save()
+#             serializer.save(book=book, user=self.request.user)
+#         else:
+#             raise serializers.ValidationError({"book": "Book is not available."})
+
+
+# class BorrowBookView(generics.CreateAPIView):
+#     queryset = Borrow.objects.all()
+#     serializer_class = BorrowSerializer
+
+#     @transaction.atomic
+#     def perform_create(self, serializer):
+#         book_id = self.kwargs.get("book_id")
+#         book = get_object_or_404(Book, pk=book_id)
+
+#         if book.available:
+#             book.available = False
+#             book.save()
+#             # When Authentication is enabled
+#             # borrow = serializer.save(book=book, user=self.request.user)
+#             borrow = serializer.save(book=book)
+
+#             data = {
+#                 "user": borrow.user.email,
+#                 "book": borrow.book.title,
+#                 "borrow_date": borrow.borrow_date.isoformat(),
+#                 "return_date": borrow.return_date.isoformat(),
+#             }
+
+#             publish_book_borrowing_message("book_borrowed", data)
+#         else:
+#             raise serializers.ValidationError({"book": "Book is not available."})
+
+
 class BorrowBookView(generics.CreateAPIView):
     queryset = Borrow.objects.all()
     serializer_class = BorrowSerializer
@@ -67,7 +96,18 @@ class BorrowBookView(generics.CreateAPIView):
         if book.available:
             book.available = False
             book.save()
-            serializer.save(book=book, user=self.request.user)
+
+            borrow = serializer.save(book=book)
+
+            data = {
+                "book_id": borrow.book.id,
+                "user": borrow.user.email,
+                "book": borrow.book.title,
+                "borrow_date": borrow.borrow_date.isoformat(),
+                "return_date": borrow.return_date.isoformat(),
+            }
+
+            publish_book_borrowing_message("book_borrowed", data)
         else:
             raise serializers.ValidationError({"book": "Book is not available."})
 
