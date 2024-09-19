3d57aca58cc58d65becc78c800f7654e7ba213c6
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-09-19 16:21:07 +0400

    Update the views to match the module paths

--- admin_api/api/views.py
+++ admin_api/api/views.py
@@ -1,11 +1,13 @@
-from rest_framework.exceptions import ValidationError
-
 from rest_framework import generics
-import requests
 
-from .models import AdminBook, User, Book
-from .serializers import AdminBookSerializer, UserSerializer, BookSerializer
-from api.helpers import publish_message_to_queue
+from .models import Borrow, User, Book
+from .serializers import (
+    BorrowSerializer,
+    UserSerializer,
+    BookSerializer,
+    UnavailableBookSerializer,
+)
+from .message_broker.publisher import publish_book_creation_message
 
 
 # # Only meant for Frontend interface only
@@ -17,31 +19,6 @@ class CreateUserFromFrontendAPI(generics.CreateAPIView):
 class AdminUserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-# class AdminBookCreateView(generics.CreateAPIView):
-#     queryset = Book.objects.all()
-#     serializer_class = BookSerializer
-
-#     def perform_create(self, serializer):
-#         book = serializer.save()
-#         frontend_api_url = "http://localhost:8000/api/books/create/"
-#         headers = {"Content-Type": "application/json"}
-#         data = {
-#             "title": book.title,
-#             "author": book.author,
-#             "publisher": book.publisher,
-#             "category": book.category,
-#             "created_at": book.created_at.isoformat(),
-#         }
-
-#         try:
-#             response = requests.post(frontend_api_url, json=data, headers=headers)
-#             response.raise_for_status()
-#         except requests.exceptions.RequestException as e:
-#             # Log the error for future investigation
-#             print(f"Error creating book in Frontend API: {e}")
-#             raise ValidationError("Failed to notify the Frontend API.")
 
 
 class AdminBookCreateView(generics.CreateAPIView):
@@ -60,25 +37,7 @@ class AdminBookCreateView(generics.CreateAPIView):
             "category": book.category,
             "created_at": book.created_at.isoformat(),
         }
-        publish_message_to_queue("book_created", data)
-
-
-# class AdminBookDeleteView(generics.DestroyAPIView):
-#     queryset = Book.objects.all()
-#     serializer_class = BookSerializer
-
-#     def perform_destroy(self, instance):
-#         book_id = instance.id
-#         frontend_api_url = f"http://localhost:8000/api/books/{book_id}/delete/"
-#         headers = {"Content-Type": "application/json"}
-
-#         try:
-#             response = requests.delete(frontend_api_url, headers=headers)
-#             response.raise_for_status()
-#             instance.delete()
-#         except requests.exceptions.RequestException as e:
-#             print(f"Error deleting book in Frontend API: {e}")
-#             raise ValidationError("Failed to delete the book in the Frontend API.")
+        publish_book_creation_message("book_created", data)
 
 
 class AdminBookDeleteView(generics.DestroyAPIView):
@@ -87,11 +46,21 @@ class AdminBookDeleteView(generics.DestroyAPIView):
 
     def perform_destroy(self, instance):
         data = {"id": instance.id}
-        publish_message_to_queue("book_deleted", data)
+        publish_book_creation_message("book_deleted", data)
 
         super().perform_destroy(instance)
 
 
-class AdminBorrowedBooksView(generics.ListAPIView):
-    queryset = AdminBook.objects.filter(available=False)
-    serializer_class = AdminBookSerializer
+# class AdminBorrowedBooksView(generics.ListAPIView):
+#     queryset = AdminBook.objects.filter(available=False)
+#     serializer_class = AdminBookSerializer
+
+
+class UserBorrowedBooksListView(generics.ListAPIView):
+    queryset = Borrow.objects.select_related("user", "book")
+    serializer_class = BorrowSerializer
+
+
+class UnavailableBooksListView(generics.ListAPIView):
+    queryset = Borrow.objects.filter(book__available=False).select_related("book")
+    serializer_class = UnavailableBookSerializer
