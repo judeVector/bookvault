8e8be88be6f8fed4268182342a6dca3855508a31
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-09-20 19:49:04 +0400

    Add messages used for consuming data in rabbitMQ

---
+++ frontend_api/api/messages/messages.py
@@ -0,0 +1,28 @@
+import json
+
+from api.models import Book
+
+
+def handle_book_creation_message(ch, method, properties, body):
+    message = json.loads(body)
+    event = message["event"]
+    data = message["data"]
+
+    if event == "book_created":
+        # Add book to frontend database
+        Book.objects.create(
+            id=data["id"],
+            title=data["title"],
+            author=data["author"],
+            publisher=data["publisher"],
+            category=data["category"],
+            created_at=data["created_at"],
+            available=True,
+        )
+    elif event == "book_deleted":
+        # Delete book from frontend database
+        try:
+            book = Book.objects.get(id=data["id"])
+            book.delete()
+        except Book.DoesNotExist:
+            print(f"Book with ID {data['id']} does not exist")
