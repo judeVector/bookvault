ec650bd020cf401144436fe9ee85aafad92428c6
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-09-21 00:56:44 +0400

    Add django command that initiat rabbitMQ to consume messages

---
+++ admin_api/api/management/commands/consume_user_messages.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+
+from api.message_broker import consumer
+
+
+class Command(BaseCommand):
+    help = "Starts the RabbitMQ consumer"
+
+    def handle(self, *args, **kwargs):
+        consumer.consume_user_messages()
